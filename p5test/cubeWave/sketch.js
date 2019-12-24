2314482f99be6b9cbb4f1f4d3ed2c6d28eb125bf
Àngel Mariages <angel.mariages@marfeel.com>
2019-12-24 11:49:18 +0100

    wow

--- p5test/cubeWave/sketch.js
+++ p5test/cubeWave/sketch.js
@@ -4,33 +4,53 @@ let angle = 0;
 let w = 24;
 let ma;
 let maxD;
+let halfWidth, halfHeight;
 
 function setup() {
-    createCanvas(windowWidth, windowWidth, WEBGL);
-    ma = atan(1 / sqrt(2));
-    maxD = dist(0,0, 200, 200);
+	createCanvas(windowWidth, windowHeight, WEBGL);
+
+	halfWidth = windowWidth / 2;
+	halfHeight = windowHeight / 2;
+
+	ma = atan(1 / sqrt(2));
+	maxD = dist(0, 0, 200, 200);
+
+	ortho(-windowWidth, windowWidth, windowHeight, -windowHeight, -3000, 3000);
 }
 
 function draw() {
-    background(0);
-    translate(mouseX - width/2, -mouseY + width/2);
-    ortho(-windowWidth, windowWidth, windowWidth, -windowWidth, 0, 1000);
+	background(0);
+	orbitControl();
+	normalMaterial();
 
-    rotateX(QUARTER_PI);
-    rotateY(ma);
+	rotateX(QUARTER_PI);
+	rotateY(ma);
 
-    for (let z = 0; z < height; z += w) {
-        for (let x = 0; x < width; x += w) {
-            push();
-            let d = dist(x, z, width / 2, height / 2);
-            let offset = map(d, 0, maxD, -3, 3);
-            let a = angle + offset;
-            let h = floor(map(sin(a), -1, 1, 100, 300));
-            translate(x - width / 2, 0, z - height / 2);
-            normalMaterial();
-            box(w - 2, h, w - 2)
-            pop();
-        }
-    }
-    angle -= 0.1;
+	for (let z = 0; z < width; z += w) {
+		for (let x = 0; x < width; x += w) {
+			push();
+
+			let d = dist(x, z, halfWidth, halfWidth);
+
+			let offset = map(d, 0, maxD, -3, 3);
+
+			let a = angle + offset;
+			let h = floor(map(sin(a), -1, 1, 100, 300));
+
+			if (x - halfWidth == 0 && z - halfWidth == 0) {
+				if (h == 100) {
+					typeof navigator.vibrate === 'function' && navigator.vibrate(500);
+				}
+			}
+
+			translate(x - halfWidth, 0, z - halfWidth);
+			box(w - 2, h, w - 2);
+
+			pop();
+		}
+	}
+	if (angle >= 360) {
+		angle = 0.05;
+	}
+	angle += 0.05;
 }
